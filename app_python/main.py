6fdac2b50194cfbe538b5a837c7999baed3c2c9e
Maxim Fomin <m.fomin@innopolis.university>
2026-03-12 23:57:08 +0300

    chore: add Loki

--- app_python/main.py
+++ app_python/main.py
@@ -1,6 +1,8 @@
 import os
+import logging
+import time
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import uvicorn
 
 from src.routes.root import router as root_router
@@ -11,8 +13,35 @@ port = int(os.getenv("PORT", 5000))
 debug = os.getenv("DEBUG", "False").lower() == "true"
 
 app = FastAPI(debug=debug)
+
+logger = logging.getLogger("app")
+logger.setLevel(logging.INFO)
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    start = time.time()
+
+    response = await call_next(request)
+
+    duration = time.time() - start
+
+    logger.info(
+        "request",
+        extra={
+            "method": request.method,
+            "path": request.url.path,
+            "status_code": response.status_code,
+            "client_ip": request.client.host,
+            "duration": duration
+        }
+    )
+
+    return response
+
+
 app.include_router(root_router)
 app.include_router(health_router)
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host=host, port=port)
